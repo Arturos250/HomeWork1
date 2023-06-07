ff35d0ebfef09f1324cea3e9744696368c32240c
Arthur <at2006tur@gmail.com>
2023-06-07 17:27:42 +0200

    Part(3)

--- task1.java
+++ task1.java
@@ -5,10 +5,10 @@ public class task1 {
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
         if(num1 > num2){
-            System.out.println(num1 + " is greater than " + num2);
+            System.out.println(num2 + " is smaller than " + num1);
         }
         else {
-            System.out.println(num2 + " is greater than " + num1);
+            System.out.println(num1 + " is smaller than " + num2);
         }
     }
 
