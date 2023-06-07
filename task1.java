fd2a5310b513f2452771e541b9f592913e6ece4e
Arthur <at2006tur@gmail.com>
2023-06-07 21:32:26 +0200

    Part(4 parity)

--- task1.java
+++ task1.java
@@ -3,13 +3,13 @@ public class task1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-        if(num1 > num2){
-            System.out.println(num1 + " is greater than " + num2);
+        if (num1 % 2  ==  0) {
+            System.out.println("the number is even");
         }
-        else {
-            System.out.println(num2 + " is greater than " + num1);
-        }
-    }
+        else{
+            System.out.println("the number is not even");
+            }
 
+
+    }
 }
